c41fa928ebebf85f98e3b3dcc40b20e8f0529aa7
jubalandres <jubalsilva17@gmail.com>
2016-06-16 14:02:05 +1000

    anim
    anim for walking

--- player.js
+++ player.js
@@ -1,11 +1,13 @@
 var UP = 0;
-var DOWN = 1;
-var UP_LEFT = 2;
-var DOWN_LEFT = 3;
-var DOWN_RIGHT = 4;
-var UP_RIGHT = 5;
-var LEFT = 6;
-var RIGHT = 7;
+var UP_LEFT = 1;
+var UP_RIGHT = 2;
+var LEFT = 3;
+var RIGHT = 4;
+var DOWN = 5;
+var DOWN_LEFT = 6;
+var DOWN_RIGHT = 7;
+
+
 
 var ANIM_IDLE_UP = 0;
 var ANIM_WALK_UP = 1;
@@ -50,13 +52,84 @@ var ANIM_DIE_DOWN_RIGHT = 31;
 
 var Player = function() {
 	this.image = document.createElement("img");
-	this.sprite = new Sprite("saxon_swordsman.png");
-	this.sprite.buildAnimation(5, 12, 68, 66, 0.05,
+	this.sprite = new Sprite("saxon_swordsma.png");
+//UP
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[0]); this.sprite.setAnimationOffset(0,-52,-33);
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[5, 5, 10, 10, 15, 15, 20 ,20]); this.sprite.setAnimationOffset(0,-52,-33);
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[25, 30, 35, 40]); this.sprite.setAnimationOffset(0,-52,-33);		
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[45, 50, 55]); this.sprite.setAnimationOffset(0,-52,-33);	
+//UP LEFT		
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[63]); this.sprite.setAnimationOffset(0,-52,-33);
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[68, 73, 78, 83]); this.sprite.setAnimationOffset(0,-52,-33);			
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[88, 93, 98, 103]); this.sprite.setAnimationOffset(0,-52,-33);			
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[108, 113, 118]); this.sprite.setAnimationOffset(0,-52,-33);	
+//UP RIGHT	
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[1]); this.sprite.setAnimationOffset(0,-52,-33);			
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[6, 11, 16, 21]); this.sprite.setAnimationOffset(0,-52,-33);			
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[26, 31, 36, 41]); this.sprite.setAnimationOffset(0,-52,-33);			
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[46, 51, 56]); this.sprite.setAnimationOffset(0,-52,-33);	
+//LEFT	
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[62]); this.sprite.setAnimationOffset(0,-52,-33);
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[67, 67, 72, 72, 77, 77, 82, 82]); this.sprite.setAnimationOffset(0,-52,-33);
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[87, 92, 97, 102]); this.sprite.setAnimationOffset(0,-52,-33);
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[107, 112, 117]); this.sprite.setAnimationOffset(0,-52,-33);	
+//RIGHT
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[2]); this.sprite.setAnimationOffset(0,-52,-33);
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[7 , 7, 12, 12, 17, 17, 22, 22]); this.sprite.setAnimationOffset(0,-52,-33);			
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[27, 32, 37, 42]); this.sprite.setAnimationOffset(0,-52,-33);			
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[47, 52, 57]); this.sprite.setAnimationOffset(0,-52,-33);
+//DOWN
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
 			[4]); this.sprite.setAnimationOffset(0,-52,-33);
-	this.sprite.buildAnimation(5, 12, 68, 66, 0.05,
-			[4]); this.sprite.setAnimationOffset(0,-52,-33);
-	
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[9, 9, 14, 14, 19, 19, 24,24]); this.sprite.setAnimationOffset(0,-52,-33);
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[29, 34, 39, 44]); this.sprite.setAnimationOffset(0,-52,-33);
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[49, 54, 59]); this.sprite.setAnimationOffset(0,-52,-33);
+//DOWN LEFT
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[61]); this.sprite.setAnimationOffset(0,-52,-33);
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[66, 71, 76, 81]); this.sprite.setAnimationOffset(0,-52,-33);			
+	this.sprite.buildAnimation(5, 24, 73, 73,0.05,
+			[86, 91, 96, 101]); this.sprite.setAnimationOffset(0,-52,-33);
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[106, 111, 116]); this.sprite.setAnimationOffset(0,-52,-33);
+			
+//DOWN RIGHT
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[3]); this.sprite.setAnimationOffset(0,-52,-33);
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[8, 13, 18, 23]); this.sprite.setAnimationOffset(0,-52,-33);
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[28, 33, 38, 43]); this.sprite.setAnimationOffset(0,-52,-33);
+	this.sprite.buildAnimation(5, 24, 73, 73, 0.05,
+			[48, 53, 58]); this.sprite.setAnimationOffset(0,-52,-33);
 
+
+	this.direction = UP;
+	
 	this.position = new Vector2();
 	this.position.set( 35*TILE, 56*TILE );
 	 
@@ -76,23 +149,41 @@ Player.prototype.update = function(deltaTime)
 {
 	this.sprite.update(deltaTime);
 	
+	var up = false;
+	var down = false;
+	var up_left = false;
+	var down_left = false;
+	var down_right = false;
+	var up_right = false;
+	var left = false;
+	var right = false;
+	
 	if(keyboard.isKeyDown(keyboard.KEY_UP) == true)
 	{
+		up = true;
+		this.direction = LEFT;
 		this.position.y -= 2;
+		if(this.sprite.currentAnimation != ANIM_WALK_UP)
+			this.sprite.setAnimation(ANIM_WALK_UP);
 	}
 	if(keyboard.isKeyDown(keyboard.KEY_DOWN) == true)
 	{
 		this.position.y += 2;
+			if(this.sprite.currentAnimation != ANIM_WALK_DOWN)
+			this.sprite.setAnimation(ANIM_WALK_DOWN);
 	}
 	if(keyboard.isKeyDown(keyboard.KEY_LEFT) == true)
 	{
 		this.position.x -= 2;
+			if(this.sprite.currentAnimation != ANIM_WALK_LEFT)
+			this.sprite.setAnimation(ANIM_WALK_LEFT);
 	}
 	if(keyboard.isKeyDown(keyboard.KEY_RIGHT) == true)
 	{
 		this.position.x += 2;
+			if(this.sprite.currentAnimation != ANIM_WALK_RIGHT)
+			this.sprite.setAnimation(ANIM_WALK_RIGHT);
 	}
-
 }
 
 Player.prototype.draw = function()
